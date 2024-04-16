209e76168f52766f0fc6e89f89a2b1966cc4ca0d
NSOOITA <74722406+naoto-sato-ooita@users.noreply.github.com>
2024-04-17 06:38:47 +0900

    Update ViewSetting.swift

--- ViewModel/ViewSetting.swift
+++ ViewModel/ViewSetting.swift
@@ -26,7 +26,7 @@ extension MapView {
         let request = MKLocalSearch.Request()                 //プレイス検索の結果を格納
         request.naturalLanguageQuery = searchText             //serchTextの自然言語をクエリに渡す
         request.region = MKCoordinateRegion(center: locationManager.userLocation, //userLocationをセンターの座標を渡す
-                                            latitudinalMeters: 1000, longitudinalMeters: 1000)
+                                            latitudinalMeters: 500, longitudinalMeters: 500)
         
         do {
             let response = try await MKLocalSearch(request: request).start()     //自然言語と座標を渡して検索開始
@@ -37,7 +37,7 @@ extension MapView {
     }
 }
 
-//lookAroundPreviewの実装（今回不要）
+////lookAroundPreviewの実装（今回不要）
 extension LocationDetailView{
     func fetchLookAroundPreview(){
         if let mapSelection{
@@ -52,7 +52,7 @@ extension LocationDetailView{
 //locationManagerの定義（重複？）
 import CoreLocation
 
-class LocationViewModel:NSObject,ObservableObject {
+class LocationViewModel:NSObject,ObservableObject, CLLocationManagerDelegate {
     private var locationManager : CLLocationManager?
     
     init(locationManager : CLLocationManager = CLLocationManager()){
@@ -63,26 +63,25 @@ class LocationViewModel:NSObject,ObservableObject {
     }
 }
 
-//位置情報の取得許可変更時のパターン別警告 （重複,処理先を確認）
-extension LocationViewModel : CLLocationManagerDelegate{
-    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
-        switch manager.authorizationStatus{
-        case .notDetermined:
-            log = "Location authorization not determined"
-        case .restricted:
-            log = "Location authorization restricted"
-        case .denied:
-            log = "Location authorization denied"
-        case .authorizedAlways:
-            manager.requestLocation()
-            log = "Location authorization always granted"
-        case .authorizedWhenInUse:
-            manager.startUpdatingLocation()
-            log = "Location authorization when in use granted"
-        @unknown default:
-            log = "Unknown authorization status"
-            
-        }
-    }
-}
-
+////位置情報の取得許可変更時のパターン別警告 （重複,処理先を確認）
+//extension LocationViewModel : CLLocationManagerDelegate{
+//    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+//        switch manager.authorizationStatus{
+//        case .notDetermined:
+//            log = "Location authorization not determined"
+//        case .restricted:
+//            log = "Location authorization restricted"
+//        case .denied:
+//            log = "Location authorization denied"
+//        case .authorizedAlways:
+//            manager.requestLocation()
+//            log = "Location authorization always granted"
+//        case .authorizedWhenInUse:
+//            manager.startUpdatingLocation()
+//            log = "Location authorization when in use granted"
+//        @unknown default:
+//            log = "Unknown authorization status"
+//            
+//        }
+//    }
+//}
