72e88bae43b2bb0e5a47c9aebad3e0c47a70d998
NSOOITA <74722406+naoto-sato-ooita@users.noreply.github.com>
2024-04-22 16:00:45 +0900

    Add files via upload

---
+++ Models/ChatView.swift
@@ -0,0 +1,77 @@
+//
+//  ChatView.swift
+//  air
+//
+//  Created by Naoto Sato on 2024/03/24.
+//
+
+import SwiftUI
+
+struct ChatView: View {
+    @StateObject var viewModel: ChatViewModel
+    let user: User
+    
+    init(user: User){
+        self.user = user
+        self._viewModel = StateObject(wrappedValue: ChatViewModel(user: user))
+    }
+    
+    var body: some View {
+        VStack{
+            
+            ScrollView{
+                
+                //header
+                VStack{
+                    CircularProfileImageView(user: user, size: .xlarge)
+                    
+                    VStack(spacing: 4){
+                        Text(user.fullname)
+                            .font(.title3)
+                            .fontWeight(.semibold)
+                        
+                        Text("Messanger")
+                            .font(.footnote)
+                            .foregroundColor(.gray)
+                    }
+                    
+                }
+                
+                //message
+                ForEach(0 ... 16 , id: \.self){ message in
+                    ChatMessageCell(isFromCurrentUser: Bool.random())
+                }
+                
+            }
+            
+
+            //message input View
+            
+            Spacer()
+            
+            ZStack(alignment: .trailing) {
+                TextField("Message...",text : $viewModel.messageText, axis: .vertical)
+                    .padding(12)
+                    .padding(.trailing,48)
+                    .background(Color(.systemGroupedBackground))
+                    .clipShape(Capsule())
+                    .font(.subheadline)
+                
+                Button{
+                    viewModel.sendMessage()
+                    viewModel.messageText = ""
+                } label: {
+                    Text("Send")
+                        .fontWeight(.semibold)
+                }
+                .padding(.horizontal)
+                
+            }
+            .padding()
+        }
+    }
+}
+
+#Preview {
+    ChatView(user:User.MOCK_USER)
+}
---
+++ Models/InBoxRowView.swift
@@ -0,0 +1,43 @@
+//
+//  InBoxRowView.swift
+//  air
+//
+//  Created by Naoto Sato on 2024/03/24.
+//
+
+import SwiftUI
+
+struct InBoxRowView: View {
+    var body: some View {
+        
+        HStack(alignment: .top , spacing: 12){
+            
+            CircularProfileImageView(user: User.MOCK_USER, size: .medium)
+            
+            VStack(alignment: .leading, spacing: 4){
+                Text("Heath Ledger")
+                    .font(.subheadline)
+                    .fontWeight(.semibold)
+                
+                Text("Some test messeage")
+                    .font(.subheadline)
+                    .foregroundColor(.gray)
+                    .lineLimit(/*@START_MENU_TOKEN@*/2/*@END_MENU_TOKEN@*/)
+                    .frame(maxWidth: UIScreen.main.bounds.width - 100, alignment: .leading)
+            }
+            
+            HStack{
+                Text("Yesterday")
+                Image(systemName: "chevron.right")
+                
+            }
+            .font(.footnote)
+            .foregroundColor(.gray)
+        }
+        .frame(height: 72)
+    }
+}
+
+#Preview {
+    InBoxRowView()
+}
