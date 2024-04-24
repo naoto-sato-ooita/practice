5c87164eb6dd299b4d218fe12ea689b11452d1d0
NSOOITA <74722406+naoto-sato-ooita@users.noreply.github.com>
2024-04-24 16:57:07 +0900

    Update NewMessageViewModel.swift

--- MakeMessage/ViewModel/NewMessageViewModel.swift
+++ MakeMessage/ViewModel/NewMessageViewModel.swift
@@ -15,9 +15,10 @@ class NewMessageViewModel: ObservableObject{
         Task { try await fetchUsers() }
     }
     
+    //MARK - 現在ユーザを再確認して、全ユーザを読み込む
     func fetchUsers() async throws {
         guard let currentUid = Auth.auth().currentUser?.uid else { return }
         let users = try await UserService.fetchAllUsers()
-        self.users = users.filter( {$0.id != currentUid} )
+        self.users = users.filter( {$0.id != currentUid} ) //配列内の現在のユーザID以外の全ユーザを抽出usersに格納
     }
 }
