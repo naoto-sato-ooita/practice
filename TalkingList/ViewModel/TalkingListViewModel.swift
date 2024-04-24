5f7cd3e1288754009d73e4d05d304bb1fef0b3d8
NSOOITA <74722406+naoto-sato-ooita@users.noreply.github.com>
2024-04-24 21:56:09 +0900

    Update TalkingListViewModel.swift

--- TalkingList/ViewModel/TalkingListViewModel.swift
+++ TalkingList/ViewModel/TalkingListViewModel.swift
@@ -9,17 +9,17 @@ import Foundation
 import Combine
 import Firebase
 
-class TalkingListViewModel: ObservableObject {
+class TalkingListViewModel: ObservableObject { //ViewModelを監視対象にする
     @Published var currentUser: User?
     
     private var cancellables = Set<AnyCancellable>() 
     //配列、購読をキャンセルするときに利用　cancel()メソッドが用意。任意のタイミングで保持しているSubscriptionを破棄できる
     
-    init(){
-        setupSubscribers() //初期化
+    init(){ //クラスのインスタンスを生成するときに自動で呼び出されるメソッド
+        setupSubscribers()
     }
     
-    private func setupSubscribers(){ 
+    private func setupSubscribers(){ //イベントを受け取って処理をする側の人間Subscriber
         UserService.shared.$currentUser.sink { [weak self] user in //Subscriberを作成
             self?.currentUser = user
         }.store(in: &cancellables) //AnyCancellable配列に格納
